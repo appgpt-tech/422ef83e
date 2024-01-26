c61dd7684453b99a5ade917ebb8b98d5690ac4cc
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:26:28 +0200

    created on Fri Jan 26 2024 23:26:16 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/data.ts
@@ -0,0 +1,3 @@
+
+let data: any = {"defaultData":{"Books":[{"title":"1984","author":"George Orwell"},{"title":"To Kill a Mockingbird","author":"Harper Lee"},{"title":"The Great Gatsby","author":"F. Scott Fitzgerald"}]}};
+export default data;
